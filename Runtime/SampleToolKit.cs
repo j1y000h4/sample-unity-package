6f715ba8b9f4efe1bbce3e73b252129b0d1eedc1
jaehong.jeong <jaehong.jeong@im-fine.co.kr>
2022-03-04 16:26:52 +0900

    add testText
    add testText

--- Runtime/SampleToolKit.cs
+++ Runtime/SampleToolKit.cs
@@ -6,9 +6,21 @@ namespace IMFINE.Tools
 {
     public class SampleToolKit : MonoBehaviour
     {
+        private string testText = "테스트입니다.";
+
         private void Start()
         {
             Debug.Log("테스트");
+        }
+
+        private void SetTestText(string _text)
+        {
+            testText = _text;
+        }
+
+        public string _testText
+        {
+            get { return testText; }
         }
     }
 }
